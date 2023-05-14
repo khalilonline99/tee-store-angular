72820672b80ad0ba0a04e91e934877dddbd0cb98
khalil <khalilonline99@gmail.com>
2023-05-14 19:27:49 +0600

    router link added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AppModule } from './app.module';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'details', component: ProductDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
